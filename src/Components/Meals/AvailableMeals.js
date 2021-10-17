6621b39972af263e01b8ae6c467dad354e900c6b
Kunal J. Pardeshi <kunaljp16@gmail.com>
2021-10-17 20:20:49 +0530

    added error handling for the fetch method

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import MealItem from "./MealItem/MealItem";
-import classes from './AvailableMeals.module.css';
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = (props) => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpsError, setHttpError] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://food-app-data-909c4-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("<p> Something went wrong</p>;");
+      }
       const responseData = await response.json();
       const loadMeals = [];
 
@@ -25,13 +29,25 @@ const AvailableMeals = (props) => {
       setMeals(loadMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+    // this is the most traditional ways for catching the error for promise
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
-      <section className={classes['loading-indicator']}>
+      <section className={classes["loading-indicator"]}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpsError) {
+    return (
+      <section className={classes["loading-indicator"]}>
+        <p>{httpsError}</p>
       </section>
     );
   }
